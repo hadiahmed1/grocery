161aebf00cbb7466668977d12ea34ea1c095994b
Hadi <969hadiahmed@gmail.com>
2025-05-06 08:39:45 +0530

    fix: Turned off sequelize logging

--- backend/src/config/sequelizeConfig.ts
+++ backend/src/config/sequelizeConfig.ts
@@ -1,13 +1,14 @@
-import {Sequelize} from 'sequelize';
+import { Sequelize } from 'sequelize';
 const dbConnection = async () => {
-        const sequelize = new Sequelize('groceryDB', 'grocery', process.env.DB_PASSWORD as string, {
-            host: 'localhost',
-            dialect: 'mysql'
-        });
-        await sequelize.authenticate();
-        
-        console.log('Connection has been established successfully.');
-        return sequelize;
+    const sequelize = new Sequelize('groceryDB', 'grocery', process.env.DB_PASSWORD as string, {
+        host: 'localhost',
+        dialect: 'mysql',
+        logging: false
+    });
+    await sequelize.authenticate();
+
+    console.log('Connection has been established successfully.');
+    return sequelize;
 }
 
 export default await dbConnection();
