3af9de06cb2b6ab76bfb47d7b6b35452a9d4129b
Hadi <969hadiahmed@gmail.com>
2025-05-08 11:18:46 +0530

    fix(type): Request

--- backend/src/controllers/cart.controllers.ts
+++ backend/src/controllers/cart.controllers.ts
@@ -1,13 +1,14 @@
 import CartItem from '../models/cartItem.model';
 import Product from '../models/product.model';
-import { Response } from 'express';
+import {Request ,Response } from 'express';
 import ApiResponse from '../helper/ApiResponse';
 import ApiError from '../helper/ApiError';
 import asyncHandler from '../helper/asyncHandler';
 import {AuthenticatedRequest} from '../types/AuthenticatedRequest';
 import httpStatus from '../constants/httpStatusCode'
 
-export const addToCart = asyncHandler(async (req: AuthenticatedRequest, res: Response) => {
+export const addToCart = asyncHandler(async (req: Request, res: Response) => {
+    if(!req.user) throw new ApiError(httpStatus.NOT_FOUND, "User not found");
     const { product_id, count } = req.body;
     if (! await Product.findByPk(product_id)) throw new ApiError(404, "Product not found");
 
@@ -21,7 +22,8 @@ export const addToCart = asyncHandler(async (req: AuthenticatedRequest, res: Res
     return res.status(httpStatus.OK).send(new ApiResponse("Item added to cart", { cartitem }));
 });
 
-export const getCart = asyncHandler(async (req: AuthenticatedRequest, res: Response) => {
+export const getCart = asyncHandler(async (req: Request, res: Response) => {
+    if(!req.user) throw new ApiError(httpStatus.NOT_FOUND, "User not found");
     const cart = await CartItem.findAll({
         where: {
             user_id: req.user.id
