d13963d75e14f9ed41a7663d88119f4379f893a7
Hadi <969hadiahmed@gmail.com>
2025-05-08 15:20:58 +0530

    feat: route to edit existing address

--- backend/src/routes/addressRoute.ts
+++ backend/src/routes/addressRoute.ts
@@ -2,10 +2,12 @@ import express from 'express';
 import { verifyAccessToken } from '../middleware/verifyToken';
 import validateData from '../middleware/validateSchema';
 import addressCreationSchema from '../types/joi/addressCreationSchema';
-import { createAddress } from '../controllers/address.controllers';
+import { createAddress, editAddress } from '../controllers/address.controllers';
+import addressEditSchema from '../types/joi/addressEditSchema';
 
 const addressRouter = express.Router();
 
 addressRouter.post('/', verifyAccessToken, validateData(addressCreationSchema), createAddress);
+addressRouter.patch('/:id', verifyAccessToken, validateData(addressEditSchema), editAddress);
 
 export default addressRouter;
