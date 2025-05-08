2afb9e294da500d42f47a1a738fbd930eaa3f9bc
Hadi <969hadiahmed@gmail.com>
2025-05-08 18:11:10 +0530

    fix: addresSeed: 1-3 adresses for each verified user

--- backend/seed/addressSeed.ts
+++ backend/seed/addressSeed.ts
@@ -1,22 +1,26 @@
-import { faker, tr } from '@faker-js/faker';
+import { faker } from '@faker-js/faker';
 import Address from '../src/models/adress.model';
 import User from '../src/models/user.model';
 
-const addressSeed = async (n: number) => {
+const addressSeed = async () => {
     await Address.sync({ force: true });
     const users = await User.findAll({ where: { isVerified: true } });
-    for (let i = 0; i < n; i++) {
-        const address = {
-            name: faker.location.secondaryAddress(),
-            user_id: users[Math.floor(Math.random() * users.length)].id,
-            line1: faker.location.buildingNumber(),
-            line2: faker.location.streetAddress(),
-            city: faker.location.city(),
-            state: faker.location.state(),
-            pincode: faker.location.zipCode(),
+    users.forEach(user => {
+        const count = Math.floor(Math.random() * 3) + 1;//max 3 addresses per user
+        for (let i = 0; i < count; i++) {
+            const address = {
+                name: faker.location.secondaryAddress(),
+                user_id: user.id,
+                line1: faker.location.buildingNumber(),
+                line2: faker.location.streetAddress(),
+                city: faker.location.city(),
+                state: faker.location.state(),
+                pincode: faker.location.zipCode(),
+            }
+            Address.build(address).save();
         }
-        Address.build(address).save();
-    }
+    });
+
     console.log("address seed done");
 }
 
