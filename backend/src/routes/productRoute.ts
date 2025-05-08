c105e15008bbe2ce16f392418db0f8cb874a0f06
Hadi <969hadiahmed@gmail.com>
2025-05-08 11:54:32 +0530

    feat: validating req before creating product

--- backend/src/routes/productRoute.ts
+++ backend/src/routes/productRoute.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 import { createProduct, getProduct, getProductById } from '../controllers/product.controllers';
 import { verifySeller } from '../middleware/verifyToken'
+import validateData from '../middleware/validateSchema';
+import productCreationSchema from '../types/joi/productCreationSchema';
 
 const productRouter = express.Router();
 //protected routes
-productRouter.post('/', verifySeller, createProduct);
+productRouter.post('/', verifySeller, validateData(productCreationSchema), createProduct);
 //public routes
 productRouter.get('/', getProduct);
 productRouter.get('/:id', getProductById);
