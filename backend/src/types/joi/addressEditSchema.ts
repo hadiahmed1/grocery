71d3c412e83992c61b10aad88ae7da648dbdecc2
Hadi <969hadiahmed@gmail.com>
2025-05-08 15:20:32 +0530

    fix: addressEdit schema
    removed required criteria

--- backend/src/types/joi/addressEditSchema.ts
+++ backend/src/types/joi/addressEditSchema.ts
@@ -2,12 +2,12 @@ import Joi from 'joi';
 
 const addressEditSchema = Joi.object({
     name: Joi.string().max(50),
-    line1: Joi.string().required().max(50),
-    line2: Joi.string().required().max(50),
-    city: Joi.string().required().max(50),
-    state: Joi.string().required().max(50),
-    pincode: Joi.string().required().max(16),
-    landmark: Joi.string().required().max(50),
+    line1: Joi.string().max(50),
+    line2: Joi.string().max(50),
+    city: Joi.string().max(50),
+    state: Joi.string().max(50),
+    pincode: Joi.string().max(16),
+    landmark: Joi.string().max(50),
 }).unknown(false);
 
 export default addressEditSchema;
