6af3b6d2ce64c2c99107e1a4d238396b7a2195f5
Hadi <969hadiahmed@gmail.com>
2025-05-11 22:03:54 +0530

    feat: Updated cors policy

--- backend/src/config/expressConfig.ts
+++ backend/src/config/expressConfig.ts
@@ -10,7 +10,10 @@ import orderRouter from "../routes/orderRoute";
 
 const app = express();
 //middlewares
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(cookieParser());
