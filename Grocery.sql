113f65b49dba87003178ce0ba3c52b61f750ebbb
Hadi <969hadiahmed@gmail.com>
2025-05-11 18:22:44 +0530

    feat: Front end setup: Vite

--- Grocery.sql
+++ Grocery.sql
@@ -8,7 +8,9 @@ DESC products;
 SELECT * FROM products p ;
 
 DESC cartitems;
-SELECT * FROM cartitems;
+SELECT * FROM 
+cartitems c JOIN users u 
+ON c.user_id = u.id;
 
 DESC address;
 SELECT * FROM address;
