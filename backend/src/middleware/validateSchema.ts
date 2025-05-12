95c6ee9402fbabfafd263c50b0a287bddbc1cf37
Hadi <969hadiahmed@gmail.com>
2025-05-12 11:05:57 +0530

    feat: multer middleware

--- backend/src/middleware/validateSchema.ts
+++ backend/src/middleware/validateSchema.ts
@@ -6,7 +6,7 @@ import asyncHandler from "../helper/asyncHandler";
 
 const validateData = (schema: ObjectSchema) => {//function to validate req based on schema
     return asyncHandler(async (req: Request, res: Response, next: NextFunction) => {
-        const { error } = schema.validate(req.body);
+        const { error } = schema.validate(req?.body);
         if (error) //invalid request
             throw new ApiError(httpStatus.BAD_REQUEST, error.details[0].message, error.details.map(err => err.message));
         next();
