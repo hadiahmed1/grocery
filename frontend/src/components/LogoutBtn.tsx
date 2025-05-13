07ea54289bae9cf58cefac375cbd3bfe36f4f7ac
Hadi <969hadiahmed@gmail.com>
2025-05-13 12:33:12 +0530

    feat: Logout btn

---
+++ frontend/src/components/LogoutBtn.tsx
@@ -0,0 +1,17 @@
+import { Link } from "react-router-dom";
+import useUser from "../hooks/useUser";
+import axiosInstance from "../lib/axiosInstance";
+
+const LogoutBtn = () => {
+    const { setUser } = useUser()
+    const logout = () => {
+        axiosInstance.get('user/logout').then(res => {
+            console.log("logout:>>", res);
+            if (res.data.success === true) {
+                setUser(null);
+            }
+        });
+    }
+    return (<Link to='/login' onClick={() => logout()} className='text-2xl text-red-500'>Logout</Link>)
+}
+export default LogoutBtn;
