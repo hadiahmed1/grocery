f39bb9dd9ca70c3961477603957ba392efd443eb
Hadi <969hadiahmed@gmail.com>
2025-05-14 16:06:59 +0530

    feat: Toast error on accesing protected routes

--- frontend/src/components/ProtectedRoute.tsx
+++ frontend/src/components/ProtectedRoute.tsx
@@ -1,9 +1,14 @@
 import { Navigate, Outlet } from "react-router-dom";
 import useUser from "../hooks/useUser";
+import { toast } from "react-toastify";
 
 const ProtectedRoute = () => {
     const { user } = useUser();
-    return (user ? <Outlet /> : <Navigate to='/login' />)
+    if (user) return (<Outlet />)
+    else {
+        toast.error("Please Login");
+        return (<Navigate to='/login' />);
+    }
 }
 
 export default ProtectedRoute;
--- frontend/src/components/SellerProtectedrRoute.tsx
+++ frontend/src/components/SellerProtectedrRoute.tsx
@@ -1,9 +1,18 @@
 import { Navigate, Outlet } from "react-router-dom";
 import useUser from "../hooks/useUser";
+import { toast } from "react-toastify";
 
 const SellerProtectedRoute = () => {
     const { user } = useUser();
-    return ((user?.role === 'seller') ? <Outlet /> : <Navigate to='/login' />)
+    if (user?.role === "seller") return (<Outlet />)
+    else if (user) {
+        toast.error("Please Login as seller");
+        return (<Navigate to='/login' />);
+    }
+    else {
+        toast.error("Please Login");
+        return (<Navigate to='/login' />);
+    }
 }
 
 export default SellerProtectedRoute;
