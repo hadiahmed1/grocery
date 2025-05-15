b050445fe8ea1991178e8898366db2c9f6f40c3a
Hadi <969hadiahmed@gmail.com>
2025-05-15 12:31:52 +0530

    feat:SellerProductsContext

---
+++ frontend/src/contexts/SellerProductsContext.tsx
@@ -0,0 +1,53 @@
+import { createContext, useEffect, useState } from "react";
+import type ProductAttributes from "../types/product.type";
+import useUser from "../hooks/useUser";
+import axiosInstance from "../lib/axiosInstance";
+import { toast } from "react-toastify";
+import { useLocation } from "react-router-dom";
+
+type SellerProductsContextType = {
+    products: ProductAttributes[];
+    loading: boolean;
+    error: string | null;
+    refetch: () => Promise<void>;
+};
+
+export const SellerProductsContext = createContext<SellerProductsContextType | null>(null);
+
+export const MyProductsProvider = ({ children }: { children: React.ReactNode }) => {
+    const [products, setProducts] = useState<ProductAttributes[]>([]);
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState<string | null>(null);
+    const { user } = useUser();
+    const location = useLocation();
+
+    const fetchMyProducts = async () => {
+        if (!user) return;
+        setLoading(true);
+        try {
+            const res = await axiosInstance.get("product/myproducts");
+            setProducts(res.data.data.cart);
+        } catch (error) {
+            console.log(error)
+            setError("Failed to load Products");
+        } finally {
+            setLoading(false);
+        }
+    };
+
+    useEffect(() => {
+        if (error) toast.error(error);
+    }, [error]);
+
+    useEffect(() => {
+        fetchMyProducts();
+        setError(null);
+    }, [location]);
+
+
+    return (
+        <SellerProductsContext.Provider value={{ error, loading, products, refetch: fetchMyProducts }}>
+            {children}
+        </SellerProductsContext.Provider>
+    )
+}
