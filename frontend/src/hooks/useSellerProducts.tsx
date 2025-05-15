67f32ee85fa2440b66fba0bc4c4f0fbe977af70e
Hadi <969hadiahmed@gmail.com>
2025-05-15 12:46:23 +0530

    fix: hook name

--- frontend/src/hooks/useSellerProducts.tsx
+++ frontend/src/hooks/useSellerProducts.tsx
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { SellerProductsContext } from "../contexts/SellerProductsContext";
 
-export const useCart = () => {
+export const useSellerProducts = () => {
     const context = useContext(SellerProductsContext);
     if (!context) throw new Error("useSellerProducys must be used within a CartProvider");
     return context;
