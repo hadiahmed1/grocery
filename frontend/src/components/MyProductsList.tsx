bc5a19c542baabb1b0ad0f1d8aa0a11da5a64df3
Hadi <969hadiahmed@gmail.com>
2025-05-15 13:42:47 +0530

    feat: List to display product cards

---
+++ frontend/src/components/MyProductsList.tsx
@@ -0,0 +1,17 @@
+import { useSellerProducts } from "../hooks/useSellerProducts";
+import ProductCard from "./ProductCard";
+
+const MyProductList = () => {
+    const { loading, products } = useSellerProducts();
+    console.log("My Products", products);
+
+    if (loading) return (<>Loading...</>)
+    return (
+        <div>
+            <h2 className="text-3xl">My Products</h2>
+            {products.map(product => <ProductCard product={product} isMyProduct={true} />)}
+        </div>
+    )
+}
+
+export default MyProductList;
