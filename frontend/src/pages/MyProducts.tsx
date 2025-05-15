827b73c87a15a02f98ee2eb4e4d1b8c5ae3f19e5
Hadi <969hadiahmed@gmail.com>
2025-05-15 12:47:20 +0530

    feat: rendering product names

--- frontend/src/pages/MyProducts.tsx
+++ frontend/src/pages/MyProducts.tsx
@@ -1,10 +1,19 @@
 import AddProductForm from "../components/AddProductForm";
+import { useSellerProducts } from "../hooks/useSellerProducts";
 
 const MyProducts = () => {
+    const { loading, products } = useSellerProducts();
+    console.log("My Products", products);
+
+    if (loading) return (<>Loading...</>)
     return (
         <>
-            <h1 className="text-5xl">Seller Route</h1>
-            <AddProductForm />
+            <h1 className="text-5xl">Seller Dashboard</h1>
+            {/* <AddProductForm /> */}
+            <div>
+                <h2 className="text-3xl">My Products</h2>
+                {products.map(product => <p>{product.name}</p>)}
+            </div>
         </>
     )
 }
