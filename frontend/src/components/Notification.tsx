86c8a0ecfdc8a957a98bcd9ab2d3a49b68acc2eb
Hadi <969hadiahmed@gmail.com>
2025-05-22 15:41:14 +0530

    feat: Listening for private notifications

--- frontend/src/components/Notification.tsx
+++ frontend/src/components/Notification.tsx
@@ -1,9 +1,11 @@
 import { useEffect } from 'react';
 import Pusher from 'pusher-js';
+import useUser from '../hooks/useUser';
 
 const Notification = () => {
+    const { user } = useUser();
     const pusherKey = import.meta.env.VITE_PUSHER_KEY || "";
-    console.log("Pusher Key:",pusherKey)
+    console.log("Pusher Key:", pusherKey)
     useEffect(() => {
         // Initialize Pusher
         const pusher = new Pusher(pusherKey, {
@@ -11,10 +13,10 @@ const Notification = () => {
         });
 
         // Subscribe to public channel
-        const channel = pusher.subscribe('my-channel');
-
+        const channel = pusher.subscribe(user?.id || "");
+        console.log(user?.id)
         // Bind to event
-        channel.bind('my-event', function (data:object) {
+        channel.bind('notification', function (data: object) {
             console.log('Received message:', data);
             // You could update state here to display messages
         });
