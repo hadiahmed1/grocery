2b29e81478743ffa8b832fe531618a83b186e81e
Hadi <969hadiahmed@gmail.com>
2025-05-22 16:28:49 +0530

    fix: not importing app

--- backend/script.ts
+++ backend/script.ts
@@ -1,9 +1,4 @@
 import "dotenv/config";
 import './src/types/express/index.d'
-import app from "./src/config/expressConfig";
+import "./src/config/expressConfig";
 import './src/helper/updateDeliveryStatus.cron';
-import { sendRandomNotification } from './src/config/pusherConfig'
-app.get('/', (req, res) => {
-    res.send('Hello World! TS')
-});
-sendRandomNotification();
