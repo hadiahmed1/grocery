66a5b2ecfb1dd391b9231166af8a63e69658b322
Hadi <969hadiahmed@gmail.com>
2025-05-22 12:55:16 +0530

    feat: new route to signing using google

--- backend/src/routes/userRoute.ts
+++ backend/src/routes/userRoute.ts
@@ -1,6 +1,6 @@
 import express from 'express';
-import { getUser, logoutUser, register, signinUser, verifyUser } from '../controllers/user.controllers';
-import { verifyAccessToken, verifyVerificationToken } from '../middleware/verifyToken';
+import { getUser, logoutUser, register, signinUser, signinWithGoogle, verifyUser } from '../controllers/user.controllers';
+import { verifyAccessToken, verifyGoogleSignin, verifyVerificationToken } from '../middleware/verifyToken';
 import validateData from '../middleware/validateSchema';
 import signInSchema from '../types/joi/signInSchema';
 import signUpSchema from '../types/joi/signUpSchema';
@@ -8,6 +8,7 @@ import signUpSchema from '../types/joi/signUpSchema';
 const userRouter = express.Router();
 userRouter.post('/signup', validateData(signUpSchema), register);
 userRouter.post('/signin', validateData(signInSchema), signinUser);
+userRouter.post('/googleSignin', verifyGoogleSignin, signinWithGoogle);
 
 //protected routes (will have access to req.user)
 userRouter.get('/', verifyAccessToken, getUser);
