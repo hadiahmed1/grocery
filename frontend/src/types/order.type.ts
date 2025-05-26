bb0d51de7fddb4f51289c2a1b0565ddac3f1989a
Hadi <969hadiahmed@gmail.com>
2025-05-26 13:56:41 +0530

    feat: isPaidAttribute in OrderAttributes

--- frontend/src/types/order.type.ts
+++ frontend/src/types/order.type.ts
@@ -1,6 +1,7 @@
 export default interface OrderAttributes {
     id: string;
     status: 'ordered' | 'delivered' | 'cancelled',
+    isPaid: boolean,
     total: number,
     delivery_date: Date,
     createdAt: Date;
