6e240ea27636a5d1b8f1a4f9d44c8f6fba1862c2
Hadi <969hadiahmed@gmail.com>
2025-05-26 11:13:08 +0530

    feat: route to pay for order

--- backend/src/routes/orderRoute.ts
+++ backend/src/routes/orderRoute.ts
@@ -1,6 +1,6 @@
 import express from 'express';
 import { verifyAccessToken } from '../middleware/verifyToken';
-import { cancelOrder, getMyOrders, getOrder, orderCart, orderItem } from '../controllers/order.contollers';
+import { cancelOrder, getMyOrders, getOrder, orderCart, orderItem, payForOrder } from '../controllers/order.contollers';
 
 const orderRouter = express.Router();
 
@@ -9,6 +9,7 @@ orderRouter.get('/:id', verifyAccessToken, getOrder);
 orderRouter.post('/product', verifyAccessToken, orderItem);
 orderRouter.post('/cart', verifyAccessToken, orderCart);
 orderRouter.patch('/:id', verifyAccessToken, cancelOrder);
+orderRouter.post('/pay/:orderID', verifyAccessToken, payForOrder);
 
 
 export default orderRouter;
