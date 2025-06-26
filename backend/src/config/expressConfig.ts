09078ae12f9324ccd18c616aa7ec043583ed367a
Hadi <969hadiahmed@gmail.com>
2025-06-26 13:32:01 +0530

    fix: socket

--- backend/src/config/expressConfig.ts
+++ backend/src/config/expressConfig.ts
@@ -42,4 +42,20 @@ const port = process.env.PORT;
 server.listen(port, () => {
   console.log(`Example app listening on port http://localhost:${port}`)
 });
-export default server;
+
+import { Server } from "socket.io";
+
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST'],
+    credentials: true
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log("Socket:>>",socket.id);
+  console.log('A USER CONNECTED');
+});
+
+export default io;
